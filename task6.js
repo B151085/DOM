a0a749f7e377edec322971b84cf183560cbe6dd1
Ramachary <60394220+B151085@users.noreply.github.com>
2023-07-09 19:37:42 +0530

    task6.js file is added
    task6.js file is added to task6.index

---
+++ task6.js
@@ -0,0 +1,10 @@
+// var li=document.querySelector('.list-group-item');
+
+// console.log(li);
+
+var li=document.querySelector('.list-group-item:nth-child(2)');
+li.style.backgroundColor='green';
+
+var list=document.querySelector('.list-group-item:nth-child(3)');
+list.style.display='none';
+
